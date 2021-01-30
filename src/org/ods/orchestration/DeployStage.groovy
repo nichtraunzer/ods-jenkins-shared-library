8fc804412b1324006c4878857450cd183de3b610
Wais,Erhard <erhard.wais@boehringer-ingelheim.com>
2021-01-30 22:38:05 +0100

    debug logs

--- src/org/ods/orchestration/DeployStage.groovy
+++ src/org/ods/orchestration/DeployStage.groovy
@@ -57,6 +57,10 @@ class DeployStage extends Stage {
 
                 // collect log data
                 if (repo.data.logs == null) { repo.data.logs = [:] }
+                script.node {
+                    script.sh "cp -r ${standardWorkspace}/logs ${script.env.WORKSPACE}/logs" || true
+                }
+
                 repo.data.logs << [created: getLogReports(steps, repo, Project.LogReportType.CHANGES)]
                 repo.data.logs << [target: getLogReports(steps, repo, Project.LogReportType.TARGET)]
                 repo.data.logs << [state: getLogReports(steps, repo, Project.LogReportType.STATE)]
